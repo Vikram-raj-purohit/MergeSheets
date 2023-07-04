39ddd333e414487fb7ccb53adfed98748def555c
Vikram-raj-purohit <126610624+Vikram-raj-purohit@users.noreply.github.com>
2023-07-04 18:41:46 +0530

    Add files via upload

---
+++ mergeSheets.js
@@ -0,0 +1,28 @@
+const ExcelJS = require('exceljs');
+
+async function mergeSheets(filePath) {
+  // Load the workbook
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile(filePath);
+
+  // Create a new workbook for the merged sheets
+  const mergedWorkbook = new ExcelJS.Workbook();
+  const mergedSheet = mergedWorkbook.addWorksheet('Merged Sheet');
+
+  // Loop through each sheet in the original workbook
+  workbook.eachSheet((worksheet, sheetId) => {
+    // Copy the sheet data to the merged sheet
+    worksheet.eachRow((row, rowNumber) => {
+      const rowData = row.values;
+      mergedSheet.addRow(rowData);
+    });
+  });
+
+  // Save the merged workbook to a new file
+  const outputFilePath = 'merged.xlsx';
+  await mergedWorkbook.xlsx.writeFile(outputFilePath);
+  console.log(`Merged sheets saved to ${outputFilePath}`);
+}
+
+const excelFilePath = 'Book2.xlsx';
+mergeSheets(excelFilePath);
